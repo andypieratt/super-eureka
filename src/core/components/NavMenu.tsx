02f5417a75e14e06a725c0fa6165cc1bae33c2f9
andypieratt <pieratt.aw@gmail.com>
2024-09-10 22:39:59 -0600

    adding navigation to other sections

--- src/core/components/NavMenu.tsx
+++ src/core/components/NavMenu.tsx
@@ -1,14 +1,23 @@
 import LunchDiningIcon from "@mui/icons-material/LunchDining";
 import { Box, Drawer, IconButton, Typography } from "@mui/material";
 import { useState } from "react";
+import { useNavigate } from "react-router";
 
 const NavMenu = () => {
+  const navigate = useNavigate();
   const [open, setOpen] = useState<boolean>(false);
+
   const handleClick = () => {
     setOpen(true);
   };
+
   const handleClose = () => {
     setOpen(false);
+  };
+
+  const handleNavigate = (route: string) => {
+    navigate(route);
+    handleClose();
   };
 
   return (
@@ -23,16 +32,37 @@ const NavMenu = () => {
         anchor="left"
         sx={{
           width: 240,
-          mt: "50px", // Makes drawer starts below the navbar
+          mt: "50px", // Makes drawer start below the navbar
         }}
         PaperProps={{
           sx: { top: "50px" }, // Must match NavBar height
         }}
         slotProps={{
-          backdrop: { invisible: true }, // Disables when drawer is open screen darkening
+          backdrop: { invisible: true }, // Disables screen darkening when drawer is open
         }}
       >
-        <Typography sx={{ p: 2 }}>.</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 1.5,
+            mx: 2,
+            mt: 2,
+          }}
+        >
+          <Typography
+            onClick={() => handleNavigate("/resume")}
+            sx={{ cursor: "pointer" }}
+          >
+            Resume
+          </Typography>
+          <Typography
+            onClick={() => handleNavigate("/contact")}
+            sx={{ cursor: "pointer" }}
+          >
+            Contact
+          </Typography>
+        </Box>
       </Drawer>
     </Box>
   );
--- src/core/routes.tsx
+++ src/core/routes.tsx
@@ -1,11 +1,21 @@
 import { RouteObject, useRoutes } from "react-router";
 import NavContainer from "./components/NavContainer";
+import { Box } from "@mui/material";
 
 const routes: RouteObject[] = [
   {
     path: "/",
     element: <NavContainer />,
-    children: [],
+    children: [
+      {
+        path: "/resume",
+        element: <Box />, // TODO: Remove and replace with resume once built
+      },
+      {
+        path: "/contact",
+        element: <Box />, // TODO: Remove and replace with contact once built
+      },
+    ],
   },
 ];
 
